e878b46fa2f595104d02df628efa8187da9829cc
daniel martins <freitasdaniel39@gmail.com>
2020-10-01 14:48:13 -0300

    criando componentes App e Navbar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from './Navbar';
 
 const App = () => {
     return (
         <div> 
+            <Navbar></Navbar>
             <h1>Meu primeiro App</h1>
         </div>
     );
