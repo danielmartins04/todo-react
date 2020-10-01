9930fda5d5ca5bcf043081034b8beedf421f5495
daniel martins <freitasdaniel39@gmail.com>
2020-10-01 15:12:09 -0300

    listando todos

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,12 +3,29 @@ import { Todo } from '../models/Todo';
 
 const TodoList = () => {
     const todos: Todo[] = [
-        {id: 1, title: 'Ir ao supermercado', done: false},
-        {id: 2, title: 'Ir a academia', done: false}
-    ];    
+        { id: 1, title: 'Ir ao supermercado', done: false },
+        { id: 2, title: 'Ir a academia', done: false }
+    ];
 
     return (
-        <div>Todo list</div>
+        <table className="uk-table">
+            <caption>Lista de tarefas</caption>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Tarefa</th>
+                    <th></th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {
+                    todos.map(
+                        todo => (<div key={todo.id}>{todo.title}</div>)
+                    )
+                }
+            </tbody>
+        </table>
     );
 }
 
